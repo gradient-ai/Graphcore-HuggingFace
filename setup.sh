da7ff1fa78c6d2328e6a7b0ec1644f2d3b0bee9c
Rahul Trada <rahult@graphcore.ai>
2022-12-07 12:14:29 +0000

    test updates to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 # Script to be sourced on launch of the Gradient Notebook
-
+echo "Running setup.sh..."
 symlink-public-resources() {
     public_source_dir=${1}
     target_dir=${2}
@@ -30,11 +30,12 @@ export PUBLIC_DATASET_DIR="/datasets"
 # symlink exe_cache files
 while [ ! -d "${PUBLIC_DATASET_DIR}/exe_cache" ]
 do 
+    echo "Waiting for dataset to be mounted..."
     sleep 5
 done
 symlink-public-resources "${PUBLIC_DATASET_DIR}/exe_cache-huggingface" $POPLAR_EXECUTABLE_CACHE_DIR
 # symlink HF datasets
-symlink-public-resources "${PUBLIC_DATASET_DIR}/huggingface_caches/datasets" $HF_DATASETS_CACHE
+# symlink-public-resources "${PUBLIC_DATASET_DIR}/huggingface_caches/datasets" $HF_DATASETS_CACHE
 
 # Set framework specific variables
 export POPTORCH_CACHE_DIR="${POPLAR_EXECUTABLE_CACHE_DIR}"
@@ -42,3 +43,4 @@ export POPTORCH_LOG_LEVEL=ERR
 
 # pre-install the correct version of optimum for this release
 python -m pip install "optimum-graphcore>0.4, <0.5"
+echo "Finished running setup.sh. "
