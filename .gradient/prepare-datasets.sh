f365c14ab7671d5737fe9432e7910c368a91a6bd
Alexandre Payot <alexandrep@graphcore.ai>
2022-12-10 13:41:02 +0000

    Split setup script

---
+++ .gradient/prepare-datasets.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+symlink-public-resources() {
+    public_source_dir=${1}
+    target_dir=${2}
+
+    # need to wait until the dataset has been mounted (async on Paperspace's end)
+    #while [ ! -d "${PUBLIC_DATASET_DIR}/exe_cache" ]
+    while [ ! -d ${public_source_dir} ]
+    do
+        echo "Waiting for dataset to be mounted..."
+        sleep 1
+    done
+
+    echo "Symlinking - ${public_source_dir} to ${target_dir}"
+
+    # Make sure it exists otherwise you'll copy your current dir
+    mkdir -p ${public_source_dir}
+    cd ${public_source_dir}
+    find -type d -exec mkdir -p "${target_dir}/{}" \;
+    #find -type f -not -name "*.lock" -exec cp -sP "${PWD}/{}" "${target_dir}/{}" \;
+    find -type f -not -name "*.lock" -print0 | xargs -0 -P 50 -I {} sh -c "cp -sP \"${PWD}/{}\" \"${target_dir}/{}\""
+    cd -
+}
+
+echo "Starting preparation of datasets"
+# symlink exe_cache files
+exe_cache_source_dir="${PUBLIC_DATASET_DIR}/exe_cache"
+symlink-public-resources "${exe_cache_source_dir}" $POPLAR_EXECUTABLE_CACHE_DIR
+# symlink HF datasets
+for dataset in ${PUBLIC_DATASET_DIR}/*; do
+    # don't symlink the poplar executables, that's handled above
+    test "$dataset" = "$exe_cache_source_dir" && continue
+    # symlink the actual datasets
+    symlink_public_resources $dataset $HF_DATASETS_CACHE
+done
+# pre-install the correct version of optimum for this release
+python -m pip install "optimum-graphcore>0.4, <0.5"
+
+echo "Finished running setup.sh."
+# Run automated test if specified
+if [ $1 = "test" ]; then
+    #source .gradient/automated-test.sh "${@:2}"
+    source .gradient/automated-test.sh $2 $3 $4 $5 $6 $7 $8
+elif [ $2 = "test" ]; then
+    #source .gradient/automated-test.sh "${@:2}"
+    source .gradient/automated-test.sh $3 $4 $5 $6 $7 $8 $9
+fi
--- setup.sh
+++ setup.sh
@@ -3,20 +3,7 @@
 # Script to be sourced on launch of the Gradient Notebook
 
 # called from root folder in container
-
-symlink-public-resources() {
-    public_source_dir=${1}
-    target_dir=${2}
-    echo "Symlinking - ${public_source_dir} to ${target_dir}"
-
-    # Make sure it exists otherwise you'll copy your current dir
-    mkdir -p ${public_source_dir}
-    cd ${public_source_dir}
-    find -type d -exec mkdir -p "${target_dir}/{}" \;
-    #find -type f -not -name "*.lock" -exec cp -sP "${PWD}/{}" "${target_dir}/{}" \;
-    find -type f -not -name "*.lock" -print0 | xargs -0 -P 50 -I {} sh -c "cp -sP \"${PWD}/{}\" \"${target_dir}/{}\""
-    cd -
-}
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 DETECTED_NUMBER_OF_IPUS=$(python .gradient/available_ipus.py)
 
@@ -38,34 +25,9 @@ export HUGGINGFACE_HUB_CACHE="/tmp/huggingface_caches"
 export TRANSFORMERS_CACHE="/tmp/huggingface_caches/checkpoints"
 export HF_DATASETS_CACHE="/tmp/huggingface_caches/datasets"
 
-
 # Set framework specific variables
 export POPTORCH_CACHE_DIR="${POPLAR_EXECUTABLE_CACHE_DIR}"
 export POPTORCH_LOG_LEVEL=ERR
 
-prepare_datasets(){
-    echo "Starting preparation of datasets"
-    # symlink exe_cache files
-    exe_cache_source_dir="${PUBLIC_DATASET_DIR}/exe_cache"
-    symlink-public-resources "${exe_cache_source_dir}" $POPLAR_EXECUTABLE_CACHE_DIR
-    # symlink HF datasets
-    for dataset in ${PUBLIC_DATASET_DIR}/*; do
-        # don't symlink the poplar executables, that's handled above
-        test "$dataset" = "$exe_cache_source_dir" && continue
-        # symlink the actual datasets
-        symlink_public_resources $dataset $HF_DATASETS_CACHE
-    done
-    # pre-install the correct version of optimum for this release
-    python -m pip install "optimum-graphcore>0.4, <0.5"
 
-    echo "Finished running setup.sh."
-    # Run automated test if specified
-    if [ $1 = "test" ]; then
-        #source .gradient/automated-test.sh "${@:2}"
-        source .gradient/automated-test.sh $2 $3 $4 $5 $6 $7 $8
-    fi
-}
-
-nohup prepare_datasets & tail -f nohup.out &
-
-jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+nohup bash ${SCRIPT_DIR}/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
