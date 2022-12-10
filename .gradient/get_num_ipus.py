89fecefd27578083978e7ff789acb923649a00c7
Rahul Trada <rahult@graphcore.ai>
2022-12-10 13:15:49 +0000

    move automated testing into .gradient folder

---
+++ .gradient/get_num_ipus.py
@@ -0,0 +1,11 @@
+import subprocess
+import json
+import os
+
+j = subprocess.check_output(['gc-monitor', '-j'])
+data = json.loads(j)
+num_ipuMs = len(data["cards"])
+num_ipus = 4 * num_ipuMs
+
+# to be captured as a variable in the bash script that calls this python script
+print(num_ipus)
