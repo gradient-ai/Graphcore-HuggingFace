7ca40d4f408275908bb1546f83e1dfba3ab4dfea
Alexandre Payot <alexandrep@graphcore.ai>
2022-12-10 14:33:27 +0000

    Launch jupyter in setup.sh

--- setup.sh
+++ setup.sh
@@ -31,3 +31,5 @@ export POPTORCH_LOG_LEVEL=ERR
 
 
 nohup /notebooks/.gradient/prepare-datasets.sh ${@} & tail -f nohup.out &
+
+PIP_DISABLE_PIP_VERSION_CHECK=1 CACHE_DIR=/tmp DATASET_DIR=/graphcore jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
