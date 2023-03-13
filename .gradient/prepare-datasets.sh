d6e6e610edf57f5a84c8b29d75a10c745af8449d
Rahul Trada <rahult@graphcore.ai>
2023-03-13 11:05:50 +0000

    use python and json config file for symlinking

--- .gradient/prepare-datasets.sh
+++ .gradient/prepare-datasets.sh
@@ -1,32 +1,6 @@
-#!/bin/bash
-
-symlink-public-resources() {
-    public_source_dir=${1}
-    target_dir=${2}
-
-    # need to wait until the dataset has been mounted (async on Paperspace's end)
-    while [ ! -d ${public_source_dir} ] || [ -z "$(ls -A ${public_source_dir})" ]
-    do
-        echo "Waiting for dataset "${public_source_dir}" to be mounted..."
-        sleep 1
-    done
-
-    echo "Symlinking - ${public_source_dir} to ${target_dir}"
-
-    # Make sure it exists otherwise you'll copy your current dir
-    # mkdir -p ${public_source_dir}
-    # cd ${public_source_dir}
-    # find -type d -exec mkdir -p "${target_dir}/{}" \;
-    # find -type f -not -name "*.lock" -print0 | xargs -0 -P 50 -I {} sh -c "cp -sP \"${PWD}/{}\" \"${target_dir}/{}\""
-    # cd -
-    mkdir -p ${target_dir}
-    workdir="/fusedoverlay/workdirs/${public_source_dir}"
-    upperdir="/fusedoverlay/upperdir/${public_source_dir}"
-    mkdir -p ${workdir}
-    mkdir -p ${upperdir}
-    fuse-overlayfs -o lowerdir=${public_source_dir},upperdir=${upperdir},workdir=${workdir} ${target_dir}
-
-}
+#! /usr/bin/env bash 
+set -u 
+set -o pipefail
 
 if [ ! "$(command -v fuse-overlayfs)" ]
 then
@@ -37,24 +11,18 @@ fi
 
 
 echo "Starting preparation of datasets"
-# symlink exe_cache files
-exe_cache_source_dir="${PUBLIC_DATASET_DIR}/poplar-executables-hf-3-1"
-symlink-public-resources "${exe_cache_source_dir}" $POPLAR_EXECUTABLE_CACHE_DIR
-# symlink HF datasets
-HF_DATASETS="conll2003 glue imagefolder librispeech_asr squad swag wikitext wmt16 xsum"
-for dataset in ${HF_DATASETS}; do
-    # symlink the actual datasets
-    symlink-public-resources "${PUBLIC_DATASET_DIR}/${dataset}" "${HF_DATASETS_CACHE}/$(basename ${dataset})"
-done
-# Image classification dataset
-symlink-public-resources "${PUBLIC_DATASET_DIR}/dfki-sentinel-eurosat" "${DATASET_DIR}/dfki-sentinel-eurosat"
+cd "$(dirname "$0")"
+python -u ./symlink_datasets_and_caches.py
+cd -
+
+
 # pre-install the correct version of optimum for this release
 python -m pip install "optimum-graphcore>=0.5, <0.6"
 
 echo "Finished running setup.sh."
 # Run automated test if specified
-if [[ "$1" == "test" ]]; then
+if [[ "${1:-}" == "test" ]]; then
     bash /notebooks/.gradient/automated-test.sh "${@:2}"
-elif [[ "$2" == "test" ]]; then
+elif [[ "${2:-}" == "test" ]]; then
     bash /notebooks/.gradient/automated-test.sh "${@:3}"
 fi
---
+++ .gradient/symlink_datasets_and_caches.py
@@ -0,0 +1,54 @@
+import json
+import time
+from pathlib import Path
+import subprocess
+import os
+
+
+# read in symlink config file
+with open(f"{Path(__file__).parent.absolute().as_posix()}/symlink_config.json", "r") as f:
+    json_data = f.read()
+
+# substitute environment variables in the JSON data
+json_data = os.path.expandvars(json_data)
+# parse the json data
+config = json.loads(json_data)
+
+# loop through each key-value pair
+# the key is the target directory, the value is a list of source directories
+for target_dir, source_dirs_list in config.items():
+    # need to wait until the dataset has been mounted (async on Paperspace's end)
+    source_dirs_exist_paths = []
+    for source_dir in source_dirs_list:
+        source_dir_path = Path(source_dir)
+        COUNTER = 0
+        # wait until the dataset exists and is populated/non-empty, with a 300s/5m timeout
+        while (COUNTER < 300) and (not source_dir_path.exists() or not any(source_dir_path.iterdir())):
+            print(f"Waiting for dataset {source_dir_path.as_posix()} to be mounted...")
+            time.sleep(1)
+            COUNTER += 1
+
+        # dataset doesn't exist after 300s, so skip it
+        if COUNTER == 300:
+            print(f"Abandoning symlink! - source dataset {source_dir} has not been mounted & populated after 5 minutes.")
+            break
+        else:
+            print(f"Found dataset {source_dir}")
+            source_dirs_exist_paths.append(source_dir)
+    
+    # create overlays for source dataset dirs 
+    if len(source_dirs_exist_paths) > 0:
+        print(f"Symlinking - {source_dirs_exist_paths} to {target_dir}")
+        print("-" * 100)
+
+        Path(target_dir).mkdir(parents=True, exist_ok=True)
+
+        workdir_path = Path("/fusedoverlay/workdirs" + target_dir)
+        workdir_path.mkdir(parents=True, exist_ok=True)
+        upperdir_path = Path("/fusedoverlay/upperdir" + target_dir) 
+        upperdir_path.mkdir(parents=True, exist_ok=True)
+
+        lowerdirs = ":".join(source_dirs_exist_paths)
+        overlay_command = f"fuse-overlayfs -o lowerdir={lowerdirs},upperdir={upperdir_path.as_posix()},workdir={workdir_path.as_posix()} {target_dir}"
+        subprocess.run(overlay_command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
