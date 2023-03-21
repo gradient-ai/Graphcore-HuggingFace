4790f8ffc72cd1deafcc75b0c58e2d408f27bfac
Alexandre Payot <alexandrep@graphcore.ai>
2023-03-21 13:00:34 +0000

    Raise error when timeout is hit

--- .gradient/available_ipus.py
+++ .gradient/available_ipus.py
@@ -1,13 +1,22 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 import subprocess
 import json
+import warnings
+import os
+
 try:
     j = subprocess.check_output(['gc-monitor', '-j'], timeout=10)
     data = json.loads(j)
     num_ipuMs = len(data["cards"])
     num_ipus = 4 * num_ipuMs
 except subprocess.TimeoutExpired as err:
-    print(f"{err}")
-    num_ipus = 4
+    num_ipus = 0
+    print(num_ipus)
+    nb_id = os.getenv("PAPERSPACE_METRIC_WORKLOAD_ID", "unknown")
+    raise OSError(
+        "Connection to IPUs timed-out. This error indicates a problem with the "
+        "hardware you are running on. Please contact Paperspace Support referencing"
+        f" the Notebook ID: {nb_id}"
+    ) from err
 # to be captured as a variable in the bash script that calls this python script
 print(num_ipus)
