faac5ad3240fca0db1c355efe8adbdb6ff15386f
Alexandre Payot <alexandrep@graphcore.ai>
2023-03-21 12:21:23 +0000

    Add a timeout to GC-monitor

--- .gradient/available_ipus.py
+++ .gradient/available_ipus.py
@@ -1,11 +1,13 @@
 # Copyright (c) 2022 Graphcore Ltd. All rights reserved.
 import subprocess
 import json
-
-j = subprocess.check_output(['gc-monitor', '-j'])
-data = json.loads(j)
-num_ipuMs = len(data["cards"])
-num_ipus = 4 * num_ipuMs
-
+try:
+    j = subprocess.check_output(['gc-monitor', '-j'], timeout=10)
+    data = json.loads(j)
+    num_ipuMs = len(data["cards"])
+    num_ipus = 4 * num_ipuMs
+except subprocess.TimeoutExpired as err:
+    print(f"{err}")
+    num_ipus = 4
 # to be captured as a variable in the bash script that calls this python script
 print(num_ipus)
