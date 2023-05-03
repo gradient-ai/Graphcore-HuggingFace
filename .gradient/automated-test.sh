1a8c876c59bfaa22fa2590504a2c1dc688f3da4f
Eva Woodbridge <evaw@graphcore.ai>
2023-05-03 14:51:23 +0100

    Adding health check

--- .gradient/automated-test.sh
+++ .gradient/automated-test.sh
@@ -42,6 +42,12 @@ run_tests(){
     LOG_FOLDER="${5}/log_${4}_$(date +'%Y-%m-%d-%H_%M_%S')"
     TEST_CONFIG_FILE="${6}"
     mkdir -p ${LOG_FOLDER}
+    # Run the health check script
+    HEALTH_CHECK_LOG_FOLDER="/storage/graphcore_health_checks"
+    python -m examples_utils.paperspace_utils.health_check --log-folder ${HEALTH_CHECK_LOG_FOLDER}
+    # Copy the health check logs to local log folder
+    HEALTH_CHECK_LOG_FILE=$(find ${HEALTH_CHECK_LOG_FOLDER} -type f | sort -n | tail -1)
+    cp ${HEALTH_CHECK_LOG_FILE} ${LOG_FOLDER}
     cd /notebooks/
     python -m examples_utils platform_assessment --spec ${TEST_CONFIG_FILE} "${@:9}" \
         --log-dir $LOG_FOLDER \
